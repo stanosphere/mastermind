47987d2a421dd844a12e6fe2dd2d0b325a6246fd
Paul <paul@elliptic.co>
2020-08-28 22:38:25 +0100

    idempotence is bae

--- src/main/scala/mastermind/RunAlgorithm.scala
+++ src/main/scala/mastermind/RunAlgorithm.scala
@@ -8,13 +8,21 @@ object RunAlgorithm {
 
   def main(args: Array[String]): Unit =
     args.headOption match {
-      case Some("knuth")       => solveAllPossibleCodes(Knuth, outputDir + "knuth.csv")
-      case Some("brute-force") => solveAllPossibleCodes(BruteForce, outputDir + "brute-force.csv")
+      case Some("knuth")       => go(Knuth, outputDir + "knuth.csv")
+      case Some("brute-force") => go(BruteForce, outputDir + "brute-force.csv")
       case _                   => println(errorMessage)
     }
 
   val outputDir    = "out/mastermind/"
   val errorMessage = "please specify one of the algorithms found here: either `knuth` or `brute-force`"
+
+  def go(algorithm: Algorithm, path: String): Unit = {
+    deleteFileContents(path)
+    solveAllPossibleCodes(algorithm, path)
+  }
+
+  def deleteFileContents(path: String): Unit =
+    File(path).clear()
 
   def solveAllPossibleCodes(algorithm: Algorithm, path: String): Unit =
     Code.allPossibleCodes.iterator
